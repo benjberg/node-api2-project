c96bb5e80167fe8de8a273848f6c15a630ee1500
Ben <bbjberg91@gmail.com>
2020-04-07 19:33:14 -0400

    mvp

--- router.js
+++ router.js
@@ -19,14 +19,31 @@ router.get('/', (req, res) => {
 
 
   router.get('/:id', (req , res) =>{
-      Posts.findById(req.params.id).then(post=>{
-          if(post){
-          res.status(200).json(post);} 
-          else{
-              res.status(404).json({message: 'post could not be found'})};
-          })
+    Posts.findById(req.params.id)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({
+          message: "post not found",
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error processing request",
+      });
+    });
   });
 
+  router.get('/:id/comments', (req, res) =>{
+    Posts.findPostComments(req.params.id).then(comment =>{
+        res.status(200).json(comment)
+            }).catch(err=> {
+                res.status(500).json({message: 'something went wrong'})
+            })
+  })
 
   router.post('/', (req,res) => {
       Posts.insert(req.body).then(post =>{
@@ -34,6 +51,32 @@ router.get('/', (req, res) => {
       }).catch(err => {
           res.status(500).json({message: 'error adding post'})
       })
+  })
+
+  router.post('/:id/comments', (req, res) =>{
+    Posts.findById(req.params.id).then((post) => {
+      if (post) {
+        req.body.text
+          ? Posts.insertComment(req.body)
+              .then(res.status(200).json(req.body))
+              .catch((err) => {
+                console.log(err);
+              })
+          : res
+              .status(400)
+              .json({ errorMessage: "Please provide text for the comment." });
+      } else {
+        res.status(404).json({
+          message: "post not found",
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error processing request",
+      });
+    });
   })
 
   router.delete('/:id', (req, res) =>{
@@ -46,5 +89,17 @@ router.get('/', (req, res) => {
       })
       })
     
-  
+      router.put('/:id', (req, res) =>{
+        const changes = req.body;
+    Posts.update(req.params.id, changes)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: 'The post could not be found' });
+      }
+    })
+      })
+
+
    module.exports = router;
